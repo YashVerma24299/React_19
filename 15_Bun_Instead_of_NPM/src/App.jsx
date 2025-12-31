c0a9cc445e8290db5c93616a1e1e46634939ebb9
Yash <ritikverma23@gmail.com>
2026-01-01 00:00:07 +0530

    Understand Bun

---
+++ 15_Bun_Instead_of_NPM/src/App.jsx
@@ -0,0 +1,15 @@
+// https://medium.com/@MakeComputerScienceGreatAgain/bun-vs-npm-vs-yarn-a-comprehensive-guide-to-javascript-package-managers-f0ef7a4aabe4
+
+//  npm i -g bun
+//  bun create vite
+//  bun run dev / npm run dev
+
+//  Another also exist -> yarn
+
+const App = () => {
+  return (
+    <div>Bun instaed of npm</div>
+  )
+}
+
+export default App
