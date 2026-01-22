ab771276559e128cd2251d2baaa15257193eea64
Yash <ritikverma23@gmail.com>
2026-01-23 00:07:30 +0530

    Again implement the UseReducer Hook.

--- 4_Hooks/src/components/UseReducer/UseReducer.jsx
+++ 4_Hooks/src/components/UseReducer/UseReducer.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 //   const [state, dispatch] = useReducer(reducer, initialState);
+// dispatch ->  to send actions to the reducer function
+
 
 export default function UseReducer() {
   const reducer = (state, action) => {
@@ -9,7 +11,9 @@ export default function UseReducer() {
     if(action.type=="DEC"){
       return state-1;
     }
-    
+    if(action.type=="VAL"){
+      return state+ action.payload;
+    }
   };
   const [count, dispatch] = useReducer(reducer, 0);
   return (
@@ -18,6 +22,7 @@ export default function UseReducer() {
         <h3>{count}</h3>
         <button onClick={() => dispatch({ type: "INC" })}>Increament</button>
         <button onClick={() => dispatch({ type: "DEC" })}>Decreament</button>
+        <button onClick={() => dispatch({ type: "VAL", payload: 10})}>Incraese by 10</button>
       </div>
     </>
   );
