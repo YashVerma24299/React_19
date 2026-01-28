ea8f6b476f46c15dccded54bcf3865b9d82154c6
Yash <ritikverma23@gmail.com>
2026-01-28 15:37:01 +0530

    Implement Accordian

--- 17_React_Router/src/App.jsx
+++ 17_React_Router/src/App.jsx
@@ -68,3 +68,71 @@ export default App;
 
 // Use the NavLink instead of link
 // https://reactrouter.com/api/components/NavLink
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+
+// const data = [
+//   {
+//     id: 1,
+//     title: "What is React?",
+//     content: "React is a JavaScript library for building user interfaces."
+//   },
+//   {
+//     id: 2,
+//     title: "What is useState?",
+//     content: "useState is a React Hook that lets you add state to function components."
+//   },
+//   {
+//     id: 3,
+//     title: "What is useCallback?",
+//     content: "useCallback memoizes a function to prevent unnecessary re-renders."
+//   }
+// ];
+
+// const Accordion = () => {
+//   const [openId, setOpenId] = useState(null);
+
+//   const toggleAccordion = (id) => {
+//     setOpenId((prev) => (prev === id ? null : id));
+//   };
+
+//   return (
+//     <div className="max-w-xl mx-auto mt-10 space-y-3">
+//       {data.map((item) => (
+//         <div
+//           key={item.id}
+//           className="border rounded-lg overflow-hidden"
+//         >
+//           {/* Header */}
+//           <button
+//             onClick={() => toggleAccordion(item.id)}
+//             className=" flex justify-between items-center px-4 py-3 bg-gray-100 hover:bg-gray-200 font-medium"
+//           >
+//             <span>{item.title}</span>
+//             <span className="text-xl">
+//               {openId === item.id ? "-" : "+"}
+//             </span>
+//           </button>
+
+//           {/* Content */}
+//           {openId === item.id && (
+//             <div className="px-4 py-3 text-gray-600 bg-white">
+//               {item.content}
+//             </div>
+//           )}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Accordion;
